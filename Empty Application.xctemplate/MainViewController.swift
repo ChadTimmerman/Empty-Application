fecebdd1aedee799eeba6eac5fac7041a6d00e8b
Chad Timmerman <chadstimmerman@gmail.com>
2017-01-25 13:03:05 -0800

    Created Custom Template with MainViewController() and Podfile

---
+++ Empty Application.xctemplate/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
